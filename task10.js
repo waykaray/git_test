fd5010137a41fe420e84a237b47341e70984fc76
waykaray <waykaray@gmail.com>
2022-01-12 23:29:48 +0200

    task10

---
+++ task10.js
@@ -0,0 +1,11 @@
+var arr = [6, 5, 4, 3, 2, 1];
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            var temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+console.log(arr);
