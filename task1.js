931104a357be4bd6e66b9c49641a7b030e43c351
waykaray <92266340+waykaray@users.noreply.github.com>
2022-01-20 22:48:01 +0200

    task1

--- task1.js
+++ task1.js
@@ -1,2 +1,13 @@
-var a = 0; b = 0; result = (a + b < 4) ? result = true : result = false;
-console.log(result)
+function f(number) {
+    let rezult;
+    if (typeof number === 'number') {
+        rezult = number ** 3;
+    } else {
+        throw new Error('parameter type is not a Number')
+    }
+    return rezult;
+}
+let rezult1 = f(2);
+console.log(rezult1);
+let rezult2 = f('Content')
+console.log(rezult2);
